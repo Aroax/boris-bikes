6abe0c7d37eb69180df637597e391a2facf66987
Arran Oakes <arran.oakes@gmail.com>
2021-06-03 13:07:38 +0100

    added docking station capacity, Ex14

--- #feature_spec.rb
+++ #feature_spec.rb
@@ -2,3 +2,5 @@
 require '../lib/DockingStation'
 docking_station = DockingStation.new
 docking_station.release_bike
+
+20.times { docking_station.dock Bike.new }
--- lib/Bike.rb
+++ lib/Bike.rb
@@ -1,4 +1,5 @@
 class Bike
+  # attr_reader :working
 
   def initialize(working = true)
     @working = working
